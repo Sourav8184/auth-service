67d3049e21c5c00d8f6ed103dd1fe7542c8c2900
Sourav <ksourav662@gmail.com>
2026-04-05 23:26:58 +0530

    feat: add global error handler middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { errorHandler, notFoundHandler } = require('./middlewares/error-middleware');
 
 const app = express();
 
@@ -9,6 +10,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
+
+// 404 handler
+app.use(notFoundHandler);
+
+// Global error handler (must be last)
+app.use(errorHandler);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Auth Service is running on http://localhost:${ServerConfig.PORT}`);
